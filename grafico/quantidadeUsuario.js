110261d756ee9f6ba2ae180051aab837936d4d5a
vitorqueiroz <vitorq924@gmail.com>
2025-09-03 08:19:42 -0300

    Update quantidadeUsuario.js

--- grafico/quantidadeUsuario.js
+++ grafico/quantidadeUsuario.js
@@ -1,25 +1,23 @@
-const url="https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json"
+async function quantidadeUsuario() {
+        const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json'
+                const res = await fetch(url)
+                const dados = await res.json()
+                const nomeDosPostos = Object.keys(dados)
+            const quantidadeTrabalhadores = Object.values(dados)
+           
 
-async function visualizarInformacoesGlobais() {
+                const data = [
+                {
+                x: nomeDosPostos,
+                y:quantidadeTrabalhadores,
+                type: 'bar'
+    }
+ ]
 
-    const resposta =await fetch(url)
-
-    const dados = await resposta.json()
-
-    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
-    const trabalhadoresMundo = (dados.total_trabalhadores_mundo/1e9)
-    const tempoDesTrabalho = parseInt (dados.tempo_medio_deslocamento_para_trabalho)
-    const minutos = Math.round ((dados.tempo_medio_deslocamento_para_trabalho - tempoDesTrabalho) *60)
-
-
-
-
-    const paragrafo= document.createElement('p')
-    paragrafo.classList.add('grafico-container_texto')
-
-    paragrafo.innerHTML= `O mundo tem <span>${pessoasMundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${trabalhadoresMundo}</span>   bilhões estão empregadas e passam em média  <span>${tempoDesTrabalho}horas</span> e <span>${minutos} minutos</span> por dia no caminho para o trabalho. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais.`
-    const container=document.getElementById('grafico-container')
-    container.appendChild(paragrafo);
+ const grafico = document.createElement('div')
+ grafico.className = 'grafico'
+ document.getElementById('grafico-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
 }
 
-visualizarInformacoesGlobais()
+quantidadeUsuario()
